6fb2ed66be89245879f7bce678764203da4df086
lucas <surajpetit@gmail.com>
2023-02-08 01:47:02 +0100

    Ajout script de lancement de tests

---
+++ Code/Mesure/mesure.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "memTool" ]; then
+
+  g++ Tests/$2.cpp -o compiled_file
+
+  python3 Outils/memTool $2
+
+
+elif [ "$1" = "logReader" ]; then
+
+  g++ Tests/$2.cpp -o compiled_file
+
+  strace -e trace=memory -f -o trace.log ./compiled_file
+
+  rm compiled_file
+
+  python3 Outils/logReader.py
+
+  rm trace.log
+
+elif [ "$1" = "alloc-write-start" ]; then
+
+  strace -e trace=memory -f -o trace.log ./Outils/alloc-write-start.sh $2 $3
+
+  python3 Outils/logReader.py
+
+  rm trace.log
+
+else
+  # sinon, on affiche un message d'erreur
+  echo "Error: Invalid argument."
+fi
