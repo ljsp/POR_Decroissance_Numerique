7f71e530c1cc3f081216d6ba14405f3330e46c34
Thomas Stavis <stavis.thomas@gmail.com>
2023-06-01 02:37:23 +0200

    mesure executing alloc/write

--- Code/Resultats/cgroupCreateGraph.py
+++ Code/Resultats/cgroupCreateGraph.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tkr
+import csv
 
 def sizeof_fmt(x, pos):
     if x < 0:
@@ -21,7 +22,8 @@ if __name__ == "__main__":
     # ax.set_title('RSS used to compile "helloword" programs by langages')
     # plt.show()
 
-    # Exec helloworld
+
+    # Execute helloworld
     # fig, ax = plt.subplots()
     # x = ["C", "C++", "Java", "CSharp", "Rust", "Python", "JS (Node)"]
     # y = [370933, 387317, 9595289, 3316449, 381583, 2908979, 8646983]
@@ -29,6 +31,128 @@ if __name__ == "__main__":
     # ax.bar(x, y, width=0.8, edgecolor="white", color=bar_colors)
     # ax.yaxis.set_major_formatter(tkr.FuncFormatter(sizeof_fmt))
     # ax.set_title('RSS used to execute "helloword" programs by langages')
+    # plt.show()
+
+
+    # Execute char_alloc_100000000_write_n for n from 0 to 100000000 by 1000000
+    # xc = []
+    # yc = []
+    # with open("cgroup/multi/CAlloc100MBWriteNC.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[1] = p[1].split(", ")
+    #     xc = [i for i in range(int(p[1][0]), int(p[1][1]) + 1, int(p[1][2]))]
+    #     L = L[1:]
+    #     yc = []
+    #     for i in L:
+    #         yc.append(int(i.split(" ")[2]))
+    # xcpp = []
+    # ycpp = []
+    # with open("cgroup/multi/CAlloc100MBWriteNC++.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[1] = p[1].split(", ")
+    #     xcpp = [i for i in range(int(p[1][0]), int(p[1][1]) + 1, int(p[1][2]))]
+    #     L = L[1:]
+    #     ycpp = []
+    #     for i in L:
+    #         ycpp.append(int(i.split(" ")[2]))
+    # xjava = []
+    # yjava = []
+    # with open("cgroup/multi/CAlloc100MBWriteNJava.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[1] = p[1].split(", ")
+    #     xjava = [i for i in range(int(p[1][0]), int(p[1][1]) + 1, int(p[1][2]))]
+    #     L = L[1:]
+    #     yjava = []
+    #     for i in L:
+    #         yjava.append(int(i.split(" ")[2]))
+    # xpython = []
+    # ypython = []
+    # with open("cgroup/multi/CAlloc100MBWriteNPython.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[1] = p[1].split(", ")
+    #     xpython = [i for i in range(int(p[1][0]), int(p[1][1]) + 1, int(p[1][2]))]
+    #     L = L[1:]
+    #     ypython = []
+    #     for i in L:
+    #         ypython.append(int(i.split(" ")[2]))
+    # fig, ax = plt.subplots()
+    # ax.plot(xc, yc, label="C", color='tab:blue')
+    # ax.plot(xcpp, ycpp, label="C++", color='tab:purple')
+    # ax.plot(xjava, yjava, label="Java", color='tab:red')
+    # ax.plot(xpython, ypython, label="Python", color='tab:pink')
+    # ax.xaxis.set_major_formatter(tkr.FuncFormatter(sizeof_fmt))
+    # ax.yaxis.set_major_formatter(tkr.FuncFormatter(sizeof_fmt))
+    # ax.set_title('RSS used to write char by size of write and by langages')
+    # plt.legend()
+    # plt.show()
+
+    
+    # Execute char_alloc_n_write_0 for n from 0 to 100000000 by 1000000
+    # xc = []
+    # yc = []
+    # with open("cgroup/multi/EAllocNWrite0C.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[0] = p[0].split(", ")
+    #     xc = [i for i in range(int(p[0][0]), int(p[0][1]) + 1, int(p[0][2]))]
+    #     L = L[1:]
+    #     yc = []
+    #     for i in L:
+    #         yc.append(int(i.split(" ")[2]))
+    # xcpp = []
+    # ycpp = []
+    # with open("cgroup/multi/EAllocNWrite0C++.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[0] = p[0].split(", ")
+    #     xcpp = [i for i in range(int(p[0][0]), int(p[0][1]) + 1, int(p[0][2]))]
+    #     L = L[1:]
+    #     ycpp = []
+    #     for i in L:
+    #         ycpp.append(int(i.split(" ")[2]))
+    # xjava = []
+    # yjava = []
+    # with open("cgroup/multi/EAllocNWrite0Java.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[0] = p[0].split(", ")
+    #     xjava = [i for i in range(int(p[0][0]), int(p[0][1]) + 1, int(p[0][2]))]
+    #     L = L[1:]
+    #     yjava = []
+    #     for i in L:
+    #         yjava.append(int(i.split(" ")[2]))
+    # xpython = []
+    # ypython = []
+    # with open("cgroup/multi/EAllocNWrite0Python.csv", 'r') as file:
+    #     L = csv.reader(file).__next__()
+    #     p = L[0]
+    #     p = p[2:-2].split("], [")
+    #     p[0] = p[0].split(", ")
+    #     xpython = [i for i in range(int(p[0][0]), int(p[0][1]) + 1, int(p[0][2]))]
+    #     L = L[1:]
+    #     ypython = []
+    #     for i in L:
+    #         ypython.append(int(i.split(" ")[2]))
+    # fig, ax = plt.subplots()
+    # ax.plot(xc, yc, label="C", color='tab:blue')
+    # ax.plot(xcpp, ycpp, label="C++", color='tab:purple')
+    # ax.plot(xjava, yjava, label="Java", color='tab:red')
+    # ax.plot(xpython, ypython, label="Python", color='tab:pink')
+    # ax.xaxis.set_major_formatter(tkr.FuncFormatter(sizeof_fmt))
+    # ax.yaxis.set_major_formatter(tkr.FuncFormatter(sizeof_fmt))
+    # ax.set_title('RSS used to allocate char by size of allocation and by langages')
+    # plt.legend()
     # plt.show()
 
     SystemExit()
